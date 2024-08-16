da94ea653b5eeec575afb59365c1676bde867115
El-Hadj Ilyes Abd-Lillah <ilyes1204@gmail.com>
2024-08-16 21:22:28 +0200

    (ui): details view (Pair.jsx) fixed

--- src/pages/Pair.tsx
+++ src/pages/Pair.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { Flex, Text } from '@mantine/core';
 import PageTransition from 'components/PageTransition';
 import useCryptoTicker from 'hooks/useRealTimeCryptoTicker';
@@ -6,21 +6,21 @@ import { AnimatedCounter } from 'react-animated-counter';
 import { useParams } from 'react-router-dom';
 import { getNumberPrecision } from 'utils/getNumberPrecision';
 
-const Counter: React.FC<{
-  value: string | number;
-  fontSize: string;
-  decimalPrecision?: number;
-  color?: string;
-  valueMovementColor?: boolean;
-}> = ({ value, fontSize, decimalPrecision, color, valueMovementColor }) => (
+const renderCounter = (
+  value: string | number,
+  fontSize: string,
+  decimalPrecision?: number,
+  color?: string,
+  valueMovementColor = true
+) => (
   <AnimatedCounter
     fontSize={fontSize}
     includeCommas
     incrementColor={
-      !valueMovementColor ? color : 'var(--mantine-color-teal-text)'
+      !valueMovementColor ? color : 'var(--mantine-color-red-text)'
     }
     decrementColor={
-      !valueMovementColor ? color : 'var(--mantine-color-red-text)'
+      !valueMovementColor ? color : 'var(--mantine-color-teal-text)'
     }
     value={Number(value)}
     decimalPrecision={decimalPrecision ?? getNumberPrecision(value, 2)}
@@ -28,25 +28,12 @@ const Counter: React.FC<{
   />
 );
 
-Counter.displayName = 'Counter'; // Ensuring the component has a display name
-
 const Pair: React.FC = () => {
   const { pair } = useParams<{ pair: string }>();
 
   const { tickerData, loading, error } = useCryptoTicker(
     pair?.toLowerCase() || ''
   );
-
-  // Memoize the renderCounter function outside of any conditionals
-  const memoizedPrice = useMemo(() => {
-    return (
-      <Counter
-        value={tickerData?.price || 'N/A'}
-        fontSize="140px"
-        color="var(--mantine-color-dark)"
-      />
-    );
-  }, [tickerData?.price]);
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
@@ -58,6 +45,7 @@ const Pair: React.FC = () => {
     highPrice = 'N/A',
     lowPrice = 'N/A',
     openPrice = 'N/A',
+    price = '0',
     prevClosePrice = 'N/A',
     quoteVolume = 'N/A',
     volume = 'N/A',
@@ -70,7 +58,7 @@ const Pair: React.FC = () => {
       <h1>{tickerSymbol}</h1>
       <Flex align="flex-end">
         <Text component="div" mt={-14}>
-          {memoizedPrice}
+          {renderCounter(price, '140px', 2, 'var(--mantine-color-dark)')}
         </Text>
       </Flex>
       <p>
